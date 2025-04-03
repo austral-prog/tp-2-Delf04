0f37566c9c9fe5c5f1f9900ed4bce35d204e2670
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-03 20:00:45 -0300

    Update change p3.py

--- change p3.py
+++ change p3.py
@@ -1,27 +1,3 @@
-#Escribir un programa que dado dos números reales que representan el gasto 
-#efectuado por una persona y la cantidad pagada, imprima en pantalla el informe de la cantidad de pesos y centavos a devolver.
-#Hay que respetar el formato del informe (incluido la prolijidad con los espacios y saltos de linea). 
-
-# Un ejemplo de un informe es:
-
-#Ingresar gasto:
-#23.75
-#Dinero recibido
-#100
-
-#Vuelto
-
-#Pesos:
-#76
-#Centavos:
-#25
-
-
- 
-    
-#5change()
-
-
 
 def change():
 
