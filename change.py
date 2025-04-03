6dc181e9c89d6e26f114cbc766abdda7eb292683
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-03 16:49:23 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,13 @@
 def change():
-    expense = 23.75
-    money = 100
+
+    gastos = float(input("Ingresar gasto correspondiente:\n")) 
+    dinero_recibido = float(input("Dinero recibido\n"))
+    vuelto = dinero_recibido - gastos
+    pesos = int(vuelto)
+    centavos = ((vuelto - pesos) * 100) 
+   
+    print("\nVuelto\n")
+    print(f"Pesos:\n{pesos}")
+    print(f"Centavos:\n{centavos}")
+
+change()
