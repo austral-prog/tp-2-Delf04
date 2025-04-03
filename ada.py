675847661e62ac35fea9f64155df61aead93daf2
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-03 15:26:33 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -2,8 +2,8 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
     
-full_name = first_name + last_name
-print(full_name.lower())
-print(full_name.title())
-print(full_name.upper())
-print("         " + full_name.lower())
+    full_name = first_name + last_name
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    print("         " + full_name.lower())
