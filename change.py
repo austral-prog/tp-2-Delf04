4b478881ca5f2f1b7a2c93cc47657ea2ccd07ae3
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-04 13:24:38 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,18 @@
-def change():
+def change()
+     expense = 23.75
+     money = 100
     
+     vuelto = expense - money 
+     centavos = int((vuelto) *100)
 
+    print("Dinero gastado")
+    print(f"{expense}")
+    print("Dinero Dado")
+    print(f"{money}")
+    print("--------------")
+    print("Vuelto")
+    print("          ")
+    print("Pesos")
+    print(f"{vuelto}")
+    print("Centavos")
+    print(f"{centavos}")
